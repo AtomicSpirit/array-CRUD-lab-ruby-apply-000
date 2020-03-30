936417614341e6120230b2dccf3dbd4fab4a7efa
Daniel Park <park.daniel1@gmail.com>
2020-03-30 21:20:31 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -25,7 +25,7 @@ array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-retrieve_element_from_index(array, index_number)
+retrieve_element_from_index[0]
 end
 
 def retrieve_first_element_from_array(array)
