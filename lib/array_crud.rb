2bf60362a8083487589133faa4c55a7f68c26847
Daniel Park <park.daniel1@gmail.com>
2020-03-30 20:52:21 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,7 +17,6 @@ add_element_to_start_of_array = []
 end
 
 def remove_element_from_end_of_array(array)
-remove_element_from_end_of_array = [array]
 remove_element_from_end_of_array.pop("arrays!")
 end
 
